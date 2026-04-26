186e0b0d811a82dece4d3e65d48ac1e67b5bf874
Adam-Yonas <ay342@cornell.edu>
2026-04-26 11:35:52 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import { useEffect } from "react";
+
+useEffect(() => {
+  fetch(`${API_BASE}/health`)
+    .then(res => res.json())
+    .then(data => console.log("Backend:", data))
+    .catch(err => console.error(err));
+}, []);
 
 const API_BASE = "https://space-debris-dashboard.onrender.com";
 
